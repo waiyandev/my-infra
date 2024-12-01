ead80ede3a280548a92607f264f3d216dc97238c
ShowTi <showti1500@gmail.com>
2024-12-01 19:34:31 +0630

    changed region

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -9,14 +9,14 @@ terraform {
   backend "s3" {
     bucket         = "hello-discord-terraform-state"
     key            = "remote-state"
-    region         = "eu-west-2"
+    region         = "ap-southeast-2"
     dynamodb_table = "state-lock"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region     = "eu-west-2"
+  region     = "ap-southeast-2"
   secret_key = ""
   access_key = ""
 }
