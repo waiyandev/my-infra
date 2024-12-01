0caeeee633dbe5eff39f62db5e188f8e038e3918
ShowTi <showti1500@gmail.com>
2024-12-01 14:49:37 +0630

    commented postgres

--- express-backend/app.js
+++ express-backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3000;
-const pool = require("./db");
+// const pool = require("./db");
 const { SearchSource } = require("jest");
 
 app.use(cors());
@@ -12,39 +12,39 @@ app.get("/api/v1/hello", (req, res) => {
     res.json({ message: "Nin Hao!" });
 });
 
-app.get("/api/v1/users", async (req, res) => {
-    try {
-        const result = await pool.query("SELECT * FROM users");
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
+// app.get("/api/v1/users", async (req, res) => {
+//     try {
+//         const result = await pool.query("SELECT * FROM users");
+//         res.json(result.rows);
+//     } catch (error) {
+//         console.error(error.message);
+//         res.status(500).json({ error: "Internal server error" });
+//     }
+// });
 
-app.post("/api/v1/users", async (req, res) => {
-    try {
-        const { name, email } = req.body;
-        const result = await pool.query(
-            "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
-            [name, email]
-        );
-        res.status(201).json({
-            id: result.rows[0].id,
-            name: name,
-            email: email,
-        });
-    } catch (error) {
-        if (error.constraint === "users_email_key") {
-            res.status(400).json({
-                error: "Email already exists. Please use a different email.",
-            });
-        } else {
-            console.error(error.message);
-            res.status(500).json({ error: "Internal server error" });
-        }
-    }
-});
+// app.post("/api/v1/users", async (req, res) => {
+//     try {
+//         const { name, email } = req.body;
+//         const result = await pool.query(
+//             "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+//             [name, email]
+//         );
+//         res.status(201).json({
+//             id: result.rows[0].id,
+//             name: name,
+//             email: email,
+//         });
+//     } catch (error) {
+//         if (error.constraint === "users_email_key") {
+//             res.status(400).json({
+//                 error: "Email already exists. Please use a different email.",
+//             });
+//         } else {
+//             console.error(error.message);
+//             res.status(500).json({ error: "Internal server error" });
+//         }
+//     }
+// });
 
 // default catch-all router
 app.use((req, res) => {
