3c653f2e9c7b88fdbbca9ceee1a295501f4c841f
ShowTi <showti1500@gmail.com>
2024-12-03 00:53:23 +0630

    changed lb

--- infrastructure/load-balancer.tf
+++ infrastructure/load-balancer.tf
@@ -2,52 +2,15 @@ resource "aws_lb" "lb" {
   name               = "hello-kixxy"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb.id]
-  subnets            = data.aws_subnets.public.ids
+  security_groups = [
+    aws_security_group.lb.id
+  ]
+  subnets = data.aws_subnets.public.ids
 
   enable_deletion_protection = false
 
   tags = {
     "lab" = "example"
-  }
-  
-}
-
-
-resource "aws_lb_target_group" "backend" {
-  name     = "backend-group"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 6
-    path                = "/api/v1/hello"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "backend_register" {
-  target_group_arn = aws_lb_target_group.backend.arn
-  target_id        = aws_instance.backend.id
-  port             = 3000
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_lb_listener" "backend" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.backend.arn
   }
 }
 
@@ -61,7 +24,7 @@ resource "aws_security_group" "lb" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_80" {
   security_group_id = aws_security_group.lb.id
   cidr_ipv4         = "0.0.0.0/0"
 
@@ -70,7 +33,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port     = 80
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_443" {
   security_group_id = aws_security_group.lb.id
   cidr_ipv4         = "0.0.0.0/0"
 
@@ -79,9 +42,76 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   to_port     = 443
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_egress_lb_all" {
+resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_3000" {
+  security_group_id = aws_security_group.lb.id
+  cidr_ipv4         = "0.0.0.0/0"
+
+  ip_protocol = "tcp"
+  from_port   = 3000
+  to_port     = 3000
+}
+
+resource "aws_vpc_security_group_egress_rule" "lb_allow_egress_all" {
   security_group_id = aws_security_group.lb.id
   cidr_ipv4         = "0.0.0.0/0"
 
   ip_protocol = -1
+}
+
+resource "aws_lb_target_group" "backend" {
+  name     = "backend"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    interval            = 5
+    timeout             = 4
+    path                = "/api/v1/hello"
+  }
+
+  deregistration_delay = 10
+}
+
+resource "aws_lb_listener" "backend" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "3000"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
+  }
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "frontend"
+  port     = 3001
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    interval            = 5
+    timeout             = 4
+    path                = "/"
+  }
+
+  deregistration_delay = 10
+}
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
 }
