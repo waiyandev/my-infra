ebb8aac380e68ff1a9ce5c849ccb83d058fc4e24
ShowTi <showti1500@gmail.com>
2024-12-05 13:52:50 +0630

    saved again on test

--- express-backend/index.test.js
+++ express-backend/index.test.js
@@ -2,10 +2,10 @@ const request = require("supertest");
 const { app, server } = require("./index");
 
 describe("GET /api/v1/hello", () => {
-  test('should return a JSON response with the message "Hello facebook!"', async () => {
+  test('should return a JSON response with the message "Hello google!"', async () => {
     const response = await request(app).get("/api/v1/hello").expect(200);
 
-    expect(response.body.message).toBe("Hello facebook!");
+    expect(response.body.message).toBe("Hello google!");
   });
 
   test('should not return a JSON response with the message "hi"', async () => {
